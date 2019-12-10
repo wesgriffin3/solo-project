3ff538f80e503a1c151fec25e3fbd12a8b11ab3a
Wes Griffin <typicalgames@gmail.com>
2019-12-10 20:37:05 +0000

    pics aligned

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,6 +1,8 @@
 
 $(document).ready(() => {
-    $('.btn').on('click', (function () {
+    // SEARCH
+    $('#search-btn').one('click', (function () {
+
         var api_url = 'https://images-api.nasa.gov'
         var key = 'vzj8JrmpKONko2DW5C4KPzOQTADnPyaRnSBCnElz'
 
@@ -8,21 +10,56 @@ $(document).ready(() => {
 
         $.get(`${api_url}/search?title=${text}`).then((data) => {
             console.log('success', data);
-            const allPics = $('#pics');
-            data.collection.items.forEach((item) => {
+            const container = $('.container');
+            const picText = $('#pictext')
+            data.collection.items.slice(0, 20).forEach((item) => {
                 //links [] => href => "https://"
-
                 var pic = $('<img/>'); // creating new img element
                 var link = item.links[0].href //get link from object
+                // var title = item.data[0].title
+                var description = item.data[0].description
 
                 pic.attr('src', link);// set src attribute to the link
 
-                // grab the pics element from html
+                // allPics.append(title);
+                container.append(pic);
+                // container.append('<ul id="des">' + description + '</ul>');  //Adds description
 
-                allPics.append(pic); // add this pic we made to the the DOM
             });
+
         });
 
     }));
+    // LOADMORE
+    $('#loadmore').one('click', (function () {
+
+        var api_url = 'https://images-api.nasa.gov'
+        var key = 'vzj8JrmpKONko2DW5C4KPzOQTADnPyaRnSBCnElz'
+
+        const text = $('#user-text').val();
+
+        $.get(`${api_url}/search?title=${text}`).then((data) => {
+            console.log('success', data);
+            const container = $('.container');
+            const picText = $('#pictext')
+            data.collection.items.slice(21, 40).forEach((item) => {
+                //links [] => href => "https://"
+                var pic = $('<img/>'); // creating new img element
+                var link = item.links[0].href //get link from object
+                // var title = item.data[0].title
+                var description = item.data[0].description
+
+                pic.attr('src', link);// set src attribute to the link
+
+                // allPics.append(title);
+                container.append(pic);  // add this pic we made to the the DOM
+                // container.append('<ul id="des">' + description + '</ul>');  //Adds description
+
+            });
+
+        });
+
+    }));
+
 });
 
